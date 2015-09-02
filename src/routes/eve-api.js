2c695882609b613ba48f0967b5cf4fc4383d0388
Thomas Richner <thomasrichner+github@gmail.com>
2015-09-02 02:31:20 +0200

    basic support for name/id conversion

--- src/routes/eve-api.js
+++ src/routes/eve-api.js
@@ -3,7 +3,7 @@ var crest = require('../services/crest');
 
 router.get('/characterId.json', function (req, res, next) {
     var actingId = req.session.actingId;
-    var characterName = req.query.name;
+    var characterName = req.query.name || req.body.name;
 
     return crest.getCharacterId(characterName)
         .then(res.json.bind(res))
